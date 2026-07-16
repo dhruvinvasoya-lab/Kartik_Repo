7af1a933042109e3894ec9d2c32b01e1775e6bd7
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-07-16 12:21:31 +0000

    Initial Version Commit

---
+++ compile-java-executable/1110/SuiteMain.java
@@ -0,0 +1,9 @@
+@Listeners(TestListener.class)
+public class SuiteMain {
+
+    public static int var_ra_rint = 2500;
+
+    public static String var_ra_rstr = "RunString";
+
+    public static String var_gstr = "GlobalString";
+}
---
+++ compile-java-executable/constants.java
@@ -0,0 +1,5 @@
+public class constants {
+	public static String var_gstr = "GlobalString";
+	public static String var_ra_rstr = "RunString";
+	public static int var_ra_rint = 2500;
+}
