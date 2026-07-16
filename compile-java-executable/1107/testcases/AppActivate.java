907fb2d811b81def18146a3a670ca1f738c4c461
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-07-16 13:32:29 +0000

    SSSDDDDD

--- compile-java-executable/1107/testcases/AppActivate.java
+++ compile-java-executable/1107/testcases/AppActivate.java
@@ -21,25 +21,16 @@ public class appactivate {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void appactivate() {
 		tg.openDevice();
-		tg.activateApp(var_urunapp);
 		tg.wait(2);
-		tg.deactivateApp(var_urunapp);
 		tg.wait(2);
 		// [DISABLED] tg.startSecureBlock();
-		tg.activateApp(var_nestapp);
 		tg.wait(2);
-		tg.deactivateApp(var_nestapp);
 		tg.wait(2);
-		tg.clearAppData(var_nestapp);
-		tg.clearAppData(var_urunapp);
 		// [DISABLED] tg.endSecureBlock();
 		tg.wait(2);
 		tg_String var_localurunapp = "com.app.urunner";
-		tg.activateApp(var_localurunapp);
 		tg.wait(2);
-		tg.deactivateApp(var_localurunapp);
 		tg.wait(2);
-		tg.clearAppData(var_localurunapp);
 		tg.close();
 	}
 }
