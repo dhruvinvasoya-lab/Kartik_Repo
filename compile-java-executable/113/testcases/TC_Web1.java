08026c89f29f4abe3877b8baac9d77bfb6b1da19
Upendra Goswami <upendra.goswami+11@testgrid.io>
2026-08-06 08:04:35 +0000

    TC

--- compile-java-executable/113/testcases/TC_Web1.java
+++ compile-java-executable/113/testcases/TC_Web1.java
@@ -25,17 +25,19 @@ public class tc_web1 {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void tc_web1() {
 		tg.openBrowser();
-		tg.navigateToUrl("https://demo.automationtesting.in/Register.html");
-		tg.wait(5);
-		tg.wait("ele_firstname196", ComparisonType.IS_VISIBLE);
-		tg.click("ele_firstname196", 1);
-		tg.wait("ele_firstname196", ComparisonType.IS_VISIBLE);
-		tg.type("ele_firstname196", "#TGITVAR.userId");
-		tg.wait("ele_lastname374", ComparisonType.IS_VISIBLE);
-		tg.click("ele_lastname374", 1);
-		tg.wait("ele_lastname374", ComparisonType.IS_VISIBLE);
-		tg.type("ele_lastname374", "#TGITVAR.title");
-		tg.wait(5);
+				tg.navigateToUrl("https://demo.automationtesting.in/Register.html");
+				tg.wait(5);
+				tg.wait("ele_firstname196", ComparisonType.IS_VISIBLE);
+				tg.click("ele_firstname196", 1);
+				tg.wait("ele_firstname196", ComparisonType.IS_VISIBLE);
+				tg.type("ele_firstname196", "#TGITVAR.userId");
+				tg.wait("ele_lastname374", ComparisonType.IS_VISIBLE);
+				tg.click("ele_lastname374", 1);
+				tg.wait("ele_lastname374", ComparisonType.IS_VISIBLE);
+				tg.type("ele_lastname374", "#TGITVAR.title");
+				tg.wait(5);
+				tg.wait("ele_radioradio171", ComparisonType.IS_VISIBLE);
+				tg.click("ele_radioradio171", 1);
 		tg.close();
 	}
 }
